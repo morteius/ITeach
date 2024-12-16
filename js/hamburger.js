b6f1e446aac3e5073634b0db41496e99bcd2e667
Dilliath <136249187+morteius@users.noreply.github.com>
2024-12-16 13:34:05 +0800

    Delete js directory

--- js/hamburger.js
+++
@@ -1,8 +0,0 @@
-// Select the hamburger menu and the nav links
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const navLinks = document.querySelector('.navbar ul');
-
-// Add click event to the hamburger menu
-hamburgerMenu.addEventListener('click', () => {
-    navLinks.classList.toggle('active'); // Toggle the 'active' class
-});
